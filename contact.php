7fd7eadaf505617f3b87778fc95c44dbca82791d
Raheel Izaz <raheelizaz3@gmial.com>
2023-06-12 18:20:52 +0500

    Issues fixed ad detaild added to pages

--- contact.php
+++ contact.php
@@ -6,8 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900&display=swap"
-    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900&display=swap" rel="stylesheet">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -25,9 +24,10 @@
 
   <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Ecoverde</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
-        aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="index.html">
+        <img src="images/logo_1.png" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="oi oi-menu"></span> Menu
       </button>
 
@@ -39,21 +39,19 @@
           <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
           <li class="nav-item"><a href="properties.html" class="nav-link">Properties</a></li>
           <!-- <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li> -->
-          <li class="nav-item active"><a href="contact.html" class="nav-link">Contact</a></li>
+          <li class="nav-item active"><a href="contact.php" class="nav-link">Contact</a></li>
         </ul>
       </div>
     </div>
   </nav>
   <!-- END nav -->
 
-  <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');"
-    data-stellar-background-ratio="0.5">
+  <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
         <div class="col-md-9 ftco-animate pb-0 text-center">
-          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
-                  class="fa fa-chevron-right"></i></a></span> <span>Contact <i class="fa fa-chevron-right"></i></span>
+          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="fa fa-chevron-right"></i></a></span> <span>Contact <i class="fa fa-chevron-right"></i></span>
           </p>
           <h1 class="mb-3 bread">Contact</h1>
         </div>
@@ -66,53 +64,58 @@
       <div class="row block-9 justify-content-center mb-5">
         <div class="col-md-8 mb-md-5">
           <h2 class="text-center">If you got any questions <br>please do not hesitate to send us a message</h2>
-          <form action="#" class="bg-light p-5 contact-form">
+          <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" class="bg-light p-5 contact-form">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Your Name">
+              <input type="text" name="name" class="form-control" placeholder="Your Name">
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Your Email">
+              <input type="email" name="email" class="form-control" placeholder="Your Email">
             </div>
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Subject">
+              <input type="text" name="subject" class="form-control" placeholder="Subject">
             </div>
             <div class="form-group">
-              <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+              <textarea name="textarea" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
             </div>
             <div class="form-group">
-              <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+              <input type="submit" name="submit" value="Send Message" class="btn btn-primary py-3 px-5">
             </div>
           </form>
-
         </div>
       </div>
       <div class="row d-flex mb-5 contact-info justify-content-center">
         <div class="col-md-8">
           <div class="row mb-5">
-            <div class="col-md-4 text-center py-4">
+            <div class="col-lg-3 text-center py-4">
               <div class="icon">
                 <span class="fa fa-map"></span>
               </div>
               <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
             </div>
-            <div class="col-md-4 text-center border-height py-4">
+            <div class="col-lg-3 text-center border-height py-4">
               <div class="icon">
                 <span class="fa fa-phone"></span>
               </div>
-              <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+              <p><span>Whatsapp:</span> <a href="tel:+923308291900">+92 330 8291900</a></p>
             </div>
-            <div class="col-md-4 text-center py-4">
+            <div class="col-lg-3 text-center border-height py-4">
+              <div class="icon">
+                <span class="fa fa-phone"></span>
+              </div>
+              <p><span>Phone:</span> <a href="tel:+923308291900">+92 330 8291900</a></p>
+            </div>
+            <div class="col-lg-3 text-center py-4">
               <div class="icon">
                 <span class="fa fa-paper-plane"></span>
               </div>
-              <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+              <p><span>Email:</span> <a href="mailto:info@yoursite.com">alfarooqestate911@gmail.com</a></p>
             </div>
           </div>
         </div>
       </div>
       <div class="row justify-content-center">
         <div class="col-md-8">
-          <div id="map" class="bg-white"></div>
+          <div id="" class="bg-white google-map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d463320.29679680173!2d66.51414607343753!3d24.874441400000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f9a4a470f0f%3A0xaa8e46bb6fdfac64!2sAl%20Farooq%20Estate%20Agency!5e0!3m2!1sen!2s!4v1686385703108!5m2!1sen!2s" width="100%" height="450px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
         </div>
       </div>
     </div>
@@ -123,12 +126,14 @@
       <div class="row mb-5">
         <div class="col-md">
           <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Ecoverde</h2>
-            <p>Far far away, behind the word mountains, far from the countries.</p>
+            <h2 class="ftco-heading-2">Al Farooq Real Estate</h2>
+            <p>He is not a full man who does not own a piece of land.</p>
             <ul class="ftco-footer-social list-unstyled mt-5">
+              <li class="ftco-animate"><a href="https://www.facebook.com/profile.php?
+                id=100088534601486&mibextid=ZbWKwL" target="_blank"><span class="fa fa-facebook"></span></a></li>
+              <li class="ftco-animate"><a href="https://instagram.com/alfarooqestate?
+                igshid=NGExMmI2YTkyZg==" target="_blank"><span class="fa fa-instagram"></span></a></li>
               <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-              <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
-              <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
             </ul>
           </div>
         </div>
@@ -171,9 +176,8 @@
               <ul>
                 <li><span class="icon fa fa-map"></span><span class="text">203 Fake St. Mountain View, San Francisco,
                     California, USA</span></li>
-                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                <li><a href="#"><span class="icon fa fa-envelope pr-4"></span><span
-                      class="text">info@yourdomain.com</span></a></li>
+                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+92 330 8291900</span></a></li>
+                <li><a href="#"><span class="icon fa fa-envelope pr-4"></span><span class="text">alfarooqestate911@gmail.com</span></a></li>
               </ul>
             </div>
           </div>
@@ -182,20 +186,18 @@
       <div class="row">
         <div class="col-md-12 text-center">
           Copyright &copy;
-          <script>document.write(new Date().getFullYear());</script> All rights reserved
+          <script>
+            document.write(new Date().getFullYear());
+          </script> All rights reserved
           </p>
         </div>
       </div>
     </div>
   </footer>
-
-
-
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
       <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
-        stroke="#F96D00" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
     </svg></div>
 
 
@@ -210,11 +212,9 @@
   <script src="js/jquery.magnific-popup.min.js"></script>
   <script src="js/jquery.animateNumber.min.js"></script>
   <script src="js/scrollax.min.js"></script>
-  <script
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
-
 </body>
 
 </html>
