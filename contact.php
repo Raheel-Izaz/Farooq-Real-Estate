40c7429e075a5649c3b3553ab1e0cbb36c04505b
Raheel Izaz <raheelizaz3@gmial.com>
2023-06-16 17:08:05 +0500

    Footer and contact for has been fixed

--- contact.php
+++ contact.php
@@ -10,6 +10,11 @@
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
   <link rel="stylesheet" href="css/animate.css">
 
   <link rel="stylesheet" href="css/owl.carousel.min.css">
@@ -66,19 +71,16 @@
           <h2 class="text-center">If you got any questions <br>please do not hesitate to send us a message</h2>
           <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" class="bg-light p-5 contact-form">
             <div class="form-group">
-              <input type="text" name="name" class="form-control" placeholder="Your Name">
+              <input type="text" name="name" required class="form-control" placeholder="Your Name">
             </div>
             <div class="form-group">
-              <input type="email" name="email" class="form-control" placeholder="Your Email">
+              <input type="email" name="email" required  class="form-control" placeholder="Your Email">
             </div>
             <div class="form-group">
-              <input type="text" name="subject" class="form-control" placeholder="Subject">
+              <textarea name="textarea" required  id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
             </div>
             <div class="form-group">
-              <textarea name="textarea" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
-            </div>
-            <div class="form-group">
-              <input type="submit" name="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+              <input type="submit" name="submit" required  value="Send Message" class="btn btn-primary py-3 px-5">
             </div>
           </form>
         </div>
@@ -90,11 +92,11 @@
               <div class="icon">
                 <span class="fa fa-map"></span>
               </div>
-              <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
+              <p><span>Address:</span>Faisal Apartment Shop #4 , KDA Scheme No.1 Mirah Muhammad Shah Road Near Spar super Store &amp; Siama Twins Tower KDA Muhammad Ali Society.</p>
             </div>
             <div class="col-lg-3 text-center border-height py-4">
               <div class="icon">
-                <span class="fa fa-phone"></span>
+                <span class="fa-brands fa-square-whatsapp" style="font-size:25px ;"></span>
               </div>
               <p><span>Whatsapp:</span> <a href="tel:+923308291900">+92 330 8291900</a></p>
             </div>
@@ -102,13 +104,13 @@
               <div class="icon">
                 <span class="fa fa-phone"></span>
               </div>
-              <p><span>Phone:</span> <a href="tel:+923308291900">+92 330 8291900</a></p>
+              <p><span>Phone:</span> <a href="tel:+92228291900">+92 228291900</a></p>
             </div>
             <div class="col-lg-3 text-center py-4">
               <div class="icon">
                 <span class="fa fa-paper-plane"></span>
               </div>
-              <p><span>Email:</span> <a href="mailto:info@yoursite.com">alfarooqestate911@gmail.com</a></p>
+              <p><span>Email:</span> <a href="mailto:alfarooqestate911@gmail.com">alfarooqestate911@gmail.com</a></p>
             </div>
           </div>
         </div>
@@ -122,78 +124,73 @@
   </section>
 
   <footer class="ftco-footer ftco-section">
-    <div class="container">
-      <div class="row mb-5">
-        <div class="col-md">
-          <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Al Farooq Real Estate</h2>
-            <p>He is not a full man who does not own a piece of land.</p>
-            <ul class="ftco-footer-social list-unstyled mt-5">
-              <li class="ftco-animate"><a href="https://www.facebook.com/profile.php?
-                id=100088534601486&mibextid=ZbWKwL" target="_blank"><span class="fa fa-facebook"></span></a></li>
-              <li class="ftco-animate"><a href="https://instagram.com/alfarooqestate?
-                igshid=NGExMmI2YTkyZg==" target="_blank"><span class="fa fa-instagram"></span></a></li>
-              <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md">
-          <div class="ftco-footer-widget mb-4 ml-md-4">
-            <h2 class="ftco-heading-2">Community</h2>
-            <ul class="list-unstyled">
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Search Properties</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>For Agents</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Reviews</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>FAQs</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md">
-          <div class="ftco-footer-widget mb-4 ml-md-4">
-            <h2 class="ftco-heading-2">About Us</h2>
-            <ul class="list-unstyled">
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Our Story</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Meet the team</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Careers</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md">
-          <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Company</h2>
-            <ul class="list-unstyled">
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About Us</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Press</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
-              <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Careers</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md">
-          <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Have a Questions?</h2>
-            <div class="block-23 mb-3">
-              <ul>
-                <li><span class="icon fa fa-map"></span><span class="text">203 Fake St. Mountain View, San Francisco,
-                    California, USA</span></li>
-                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+92 330 8291900</span></a></li>
-                <li><a href="#"><span class="icon fa fa-envelope pr-4"></span><span class="text">alfarooqestate911@gmail.com</span></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12 text-center">
-          Copyright &copy;
-          <script>
-            document.write(new Date().getFullYear());
-          </script> All rights reserved
-          </p>
-        </div>
-      </div>
-    </div>
-  </footer>
+		<div class="container">
+			<div class="row mb-5">
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Al Farooq Real Estate</h2>
+						<p>He is not a full man who does not own a piece of land.</p>
+						<ul class="ftco-footer-social list-unstyled mt-5">
+							<!-- <li class="ftco-animate"><a href="" target="_blank"><span class="fa fa-twitter"></span></a></li> -->
+							<li class="ftco-animate"><a href="https://www.facebook.com/profile.php?id=100088534601486&mibextid=ZbWKwL" target="_blank"><span class="fa fa-facebook"></span></a></li>
+							<li class="ftco-animate"><a href="https://instagram.com/alfarooqestate?igshid=NGExMmI2YTkyZg==" target="_blank"><span class="fa fa-instagram"></span></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4 ml-md-4">
+						<h2 class="ftco-heading-2">Community</h2>
+						<ul class="list-unstyled">
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Search Properties</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>For Agents</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Reviews</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>FAQs</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4 ml-md-4">
+						<h2 class="ftco-heading-2">About Us</h2>
+						<ul class="list-unstyled">
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Our Story</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Meet the team</a></li>
+							<li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Careers</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Company</h2>
+						<ul class="list-unstyled">
+							<li><a href="about.html"><span class="fa fa-chevron-right mr-2"></span>About Us</a></li>
+							<li><a href="properties.html"><span class="fa fa-chevron-right mr-2"></span>Properties</a></li>
+							<li><a href="contact.php"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md">
+					<div class="ftco-footer-widget mb-4">
+						<h2 class="ftco-heading-2">Have a Questions?</h2>
+						<div class="block-23 mb-3">
+							<ul>
+								<li><span class="icon fa fa-map"></span><span class="text">Faisal Apartment Shop #4 , KDA Scheme No.1 Mirah Muhammad Shah Road Near Spar super Store &amp; Siama Twins Tower KDA Muhammad Ali Society Karachi Pakistan.</span></li>
+								<li><a href="tel:+923308291900"><span class="icon fa fa-phone"></span><span class="text">+92 3228291900</span></a></li>
+								<li><a href="mailto:alfarooqestate911@gmail.com"><span class="icon fa fa-envelope pr-4"></span><span
+											class="text">alfarooqestate911@gmail.com</span></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-center">
+					Copyright &copy;
+					<script>document.write(new Date().getFullYear());</script> All rights reserved
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
       <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
@@ -216,5 +213,32 @@
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
 </body>
+<?php
+if (isset($_POST['submit'])) {
+  $name = filter_input(INPUT_POST,"name",FILTER_SANITIZE_SPECIAL_CHARS);
+  $email = filter_input(INPUT_POST,"email",FILTER_SANITIZE_EMAIL);
+  $msg =  filter_input(INPUT_POST,"textarea",FILTER_SANITIZE_SPECIAL_CHARS);
+
+  $to = "alfarooqestate911@gmail.com";
+
+  $res = mail($to, $name, $email, $msg);
+
+  if ($res) {
+?>
+    <script>
+      swal({
+        title: "Message Sent.",
+        text: "Thank you for contacting us",
+        icon: "success",
+        button: "OK",
+      });
+    </script>
+<?php
+  } else {
+    echo "Failed";
+  }
+}
+?>
 
 </html>
+
